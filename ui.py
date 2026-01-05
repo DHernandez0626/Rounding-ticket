09c0c38dd3f2db2c2d48377a66d99970991bfd33
DHernandez0626 <192526703+DHernandez0626@users.noreply.github.com>
2026-01-05 17:19:51 -0500

    created header for ui.py

--- ui.py
+++ ui.py
@@ -1,3 +1,12 @@
+#This program is strictly for use by HCA IT employees to automate the creation of rounding tickets.
+#Unauthorized use of this program is prohibited and may result in disciplinary action.
+#This program is provided "as is" without warranty of any kind.
+#HCA IT expressly disclaims all implied warranties including, without limitation, the implied warranties of
+#merchantability and fitness for a particular purpose. In no event shall HCA IT be liable for any damages
+#arising out of the use of or inability to use this program, even if HCA IT has been advised of the possibility of such damages.
+
+#Should any questions regarding the use or purpose of this program arise, please contact me personally at danny.hernandez9788@gmail.com and i will be glad to answer them.
+
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk, messagebox
